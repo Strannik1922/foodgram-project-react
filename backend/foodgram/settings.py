a64c4b9d20a549615c6bf1f5d5754cc2bf1f020f
Иван Шамайский <shamaiskii.ivan@gmail.com>
2022-11-07 19:51:59 +0500

    mixed host for workflow

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -10,7 +10,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'the-best-secret-key')
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['158.160.35.19', 'foodmanager.sytes.net', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['84.201.156.236', 'foodmanager.sytes.net', 'localhost', '127.0.0.1']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
